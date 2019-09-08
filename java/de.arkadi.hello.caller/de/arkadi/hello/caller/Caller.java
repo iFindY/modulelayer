4cb200e33dc7c8c0be5c909c9a2d56672ebf443d
Arkadi <arkadi.daschkewitsch@gmail.com>
2019-09-08 20:24:32 +0200

    module setup

--- java/de.arkadi.hello.caller/de/arkadi/hello/caller/Caller.java
+++ java/de.arkadi.hello.caller/de/arkadi/hello/caller/Caller.java
@@ -16,12 +16,11 @@ public class Caller {
     public void printIt() {
         System.out.println("==============");
         GoodDog good = new GoodDog();
-        System.out.println("==============");
         System.out.println(good.getDog());
-
-        BadDog bad = new BadDog();
         System.out.println("==============");
+        BadDog bad = new BadDog();
         System.out.println(bad.getDog());
+        System.out.println("==============");
         this.getClass().getModule().getLayer().modules().stream().forEach(System.out::println);
 
 
